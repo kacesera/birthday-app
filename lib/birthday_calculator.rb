cbfc50a2e9805684a19c10ff2e05cf1315a6193b
KC Thomas <thisiskcthomas@gmail.com>
2020-10-16 13:54:31 +0100

    BirthdayCalculator takes in a month

--- lib/birthday_calculator.rb
+++ lib/birthday_calculator.rb
@@ -1,2 +1,7 @@
 class BirthdayCalculator
+  attr_reader :day, :month
+  def initialize(day, month)
+    @day = day
+    @month = month
+  end
 end
--- spec/birthday_calculator_spec.rb
+++ spec/birthday_calculator_spec.rb
@@ -1,4 +1,14 @@
 require 'birthday_calculator'
 
 describe BirthdayCalculator do
+
+  describe '#initialize' do
+    before do
+      @birthday = BirthdayCalculator.new('23', 'October')
+    end
+
+    it 'takes in a month' do
+      expect(@birthday.day).to eq '23'
+    end
+  end
 end
