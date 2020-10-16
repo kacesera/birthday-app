90388a59548b4f01e017200b70c1bc5774d06dae
KC Thomas <thisiskcthomas@gmail.com>
2020-10-16 16:14:26 +0100

    returns false if birthday isn't today

--- lib/birthday_calculator.rb
+++ lib/birthday_calculator.rb
@@ -4,4 +4,25 @@ class BirthdayCalculator
     @day = day
     @month = month
   end
+
+  def birthday_today?
+    today_date
+    same_day? && same_month?
+  end
+
+  private
+
+  def same_day?
+    @today_day == @day
+  end
+
+  def same_month?
+    @today_month == @month
+  end
+
+  def today_date
+    time = Time.new
+    @today_day = time.strftime('%d')
+    @today_month = time.strftime('%B')
+  end
 end
--- spec/birthday_calculator_spec.rb
+++ spec/birthday_calculator_spec.rb
@@ -1,18 +1,30 @@
 require 'birthday_calculator'
 
 describe BirthdayCalculator do
+  let(:time) { Time.now }
+  let(:day) { time.strftime('%d') }
+  let(:month) { time.strftime('%B') }
+  let(:birthday) { BirthdayCalculator.new('23', 'October') }
 
   describe '#initialize' do
-    before do
-      @birthday = BirthdayCalculator.new('23', 'October')
-    end
-
     it 'takes in a month' do
-      expect(@birthday.month).to eq 'October'
+      expect(birthday.month).to eq 'October'
     end
-
     it 'takes in a day' do
-      expect(@birthday.day).to eq '23'
+      expect(birthday.day).to eq '23'
     end
   end
+
+  describe '#birthday_today?' do
+    it 'confirms if the dates match' do
+      today_birthday = BirthdayCalculator.new(day, month)
+      expect(today_birthday.birthday_today?).to be true
+    end
+
+    it 'returns false when dates do not match' do
+      allow(birthday).to receive(:same_day?) { false }
+      expect(birthday.birthday_today?).to be false
+    end
+  end
+
 end
